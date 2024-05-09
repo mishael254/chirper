e1c01b837234f9d75231d900f54428f58b6702b1
mishael254 <mishaelmishantoz@gmail.com>
2024-05-09 15:01:36 +0300

    create a new event that is going to handle the notifications

--- app/Notifications/NewChirp.php
+++ app/Notifications/NewChirp.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Notifications;
-
+use App\Models\Chirp;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Str;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -14,7 +15,7 @@ class NewChirp extends Notification
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(public Chirp $chirp)
     {
         //
     }
@@ -35,9 +36,11 @@ class NewChirp extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->subject("New chirp from {$this->chirp->user->name }")
+                    ->greeting("New Chirp from {$this->chirp->user->name} ")
+                    ->line(Str::limit($this->chirp->message, 50 ))
+                    ->action("Go to Chirper", url("/"))
+                    ->line("Thankyou for using our application");
     }
 
     /**
