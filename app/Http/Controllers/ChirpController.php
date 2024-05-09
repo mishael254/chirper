b27c1bb285412f851ba50975326e54688b38c66e
mishael254 <mishaelmishantoz@gmail.com>
2024-05-09 12:13:09 +0300

    create an authorization policy which allow a chirp only to get edited by an authorized user

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,6 +6,7 @@ use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 
 class ChirpController extends Controller
 {
@@ -67,17 +68,34 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Chirp $chirp)
+    /**
+     * public function edit(Chirp $chirp)
     {
         //
     }
+     */
+    public function edit(Chirp $chirp): View
+    {
+        Gate::authorize("update", $chirp);
+
+        return view("chirps.edit",["chirp" => $chirp]);
+    }
+    
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
         //
+        Gate::authorize("update", $chirp);
+
+        $validated = $request->validate([
+            "message"=> "required|string|max:255",
+        ]);
+
+        $chirp->update($validated);
+        return redirect(route("chirps.index"));
     }
 
     /**
--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -27,7 +27,7 @@
                                 <span class="text-gray-800">{{ $chirp->user->name }}</span>
                                 <small class="ml-2 text-sm text-gray-600">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
                                 @unless ($chirp->created_at->eq($chirp->updated_at))
-                                    <small class="text-sm text-gray-600">&middot;{{ __('edited') }} </small>
+                                    <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
                                 @endunless
                             </div>
                             @if ($chirp->user->is(auth()->user()))
